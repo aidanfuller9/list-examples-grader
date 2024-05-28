561f3e25bbd9de0daab69651cf5934519008dd97
aidanfuller9 <60492278+aidanfuller9@users.noreply.github.com>
2024-05-28 16:06:56 -0700

    Update TestListExamples.java

--- TestListExamples.java
+++ TestListExamples.java
@@ -9,6 +9,21 @@ class IsMoon implements StringChecker {
   }
 }
 
+class ContainsE implements StringChecker {
+  public boolean checkString(String s) {
+    return s.contains("e");
+  }
+}
+
+
+class LongWordChecker implements StringChecker{
+  public boolean checkString(String s){
+  return s.length() >= 5;
+  }
+}
+
+
+
 public class TestListExamples {
   @Test(timeout = 500)
   public void testMergeRightEnd() {
@@ -18,4 +33,33 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+
+  @Test(timeout = 500)
+  public void testMergeLeftEnd() {
+    List<String> left = Arrays.asList("a", "b", "c");
+    List<String> right = Arrays.asList("a", "d");
+    List<String> merged = ListExamples.merge(right, left);
+    List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
+    assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testFilter() {
+    List<String> input = Arrays.asList("bat", "battery", "charter", "professor", "drunk");
+    List<String> expected = Arrays.asList("battery", "charter", "professor");
+    assertEquals(expected, ListExamples.filter(input, new ContainsE()));
+  }
+
+  @Test(timeout = 500)
+  public void testMergeEmpty() {
+    List<String> input1 = Arrays.asList(" ");
+    List<String> input2 = Arrays.asList(" ");
+    List<String> merged = ListExamples.merge(input1, input2);
+    List<String> expected = Arrays.asList(" ");
+    assertEquals(expected, merged);
+
+
+
+  }
+
 }
